7b60479ba27b9850fc67dec4c371de3ea566ee76
Maxime Fabre <ehtnam6@gmail.com>
2017-03-08 23:39:47 +0100

    Fix path prefix on unix

--- src/Services/Filesystem/AbsoluteLocal.php
+++ src/Services/Filesystem/AbsoluteLocal.php
@@ -24,6 +24,6 @@ class AbsoluteLocal extends Local
      */
     public function applyPathPrefix($path)
     {
-        return $path;
+        return PHP_OS === 'Linux' ? '/'.$path : $path;
     }
 }
